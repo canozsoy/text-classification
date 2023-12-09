8a2c29e0953585d09eecedd3c31da284d1d1d4fe
Can Ozsoy <cozsoy@altayer.com>
2023-12-09 19:22:54 +0300

    task1 almost done

--- 784_A1_TASK1_template.py
+++ 784_A1_TASK1_template.py
@@ -1,93 +1,83 @@
+from typing import List
 import nltk
-#import sklearn
-import string
+from sklearn.metrics import accuracy_score, confusion_matrix, recall_score, precision_score, f1_score
 import pickle	# this is for saving and loading your trained classifiers.
-
-#from sklearn.svm import SVC
-from nltk.classify.scikitlearn import SklearnClassifier
-
-##############################################################
-#		If you need, you may import other packages etc.      #
-##############################################################
+import re
+from nltk.corpus import stopwords
+from sklearn.svm import SVC
+import json
+import os
 
 
+nltk.download("wordnet")
+nltk.download('stopwords')
 
+stopWords = set(stopwords.words("english"))
+	
+def removeSingleAndEmptyChar(words: List[str]):
+	singleCharacterPattern = r'^[\w]{1}$'
+	emptyStringPattern = r'^$'
+	for word in words:
+		if re.search(singleCharacterPattern, word) or re.search(emptyStringPattern, word):
+			words.remove(word)	
+	
+def lowercaseStemAndLemmatize(token, stemmer, lemmatizer):
+	return lemmatizer.lemmatize(stemmer.stem(token.lower()))
 
+def sanitizeTokens(tokens: List[str], stemmer, lemmatizer):
+	return [lowercaseStemAndLemmatize(token, stemmer, lemmatizer) for token in tokens if token.isalpha() and token.lower() not in stopWords]
+	
 
-### ***** YOU MAY ADD NEW FUNCTIONS, MODIFY OR DELETE EXISTING ONES. YOU MAY EVEN TOTALLY DISCARD THIS TEMPLATE AND CODE YOUR OWN SOLUTION FROM SCRATCH. ***** ###
-
-
-
-
-
-def preprocess(filename):	#filename is of type string. example call: preprocess("philosophy_test.txt")	
-	####################################################################################################################
-	#		TO DO: Apply basic text processing steps which you think are required. (tokenizationi stemming etc.)       #
-	#  			   If you want to do further preprocessing (e.g. removing number etc.), you can apply those here.      #
-	#																												   #
-	####################################################################################################################
-
-	filepath = filename					# you might need to change filepath depending where do you store your data files.
-	file = open(filepath, 'r')			# 'r' for read
-	lines = file.read().splitlines()	# lines is a list holding each line of your file as strings. e.g. lines = ["This is the 1st line of the file", "This is the 2nd line of the file", ...]
+def preprocess(filename, assignedClass):				
+	file = open(filename, 'r')			
+	lines = file.read().splitlines()
 	file.close()
+	
+	# Tokenize, stem, lemmatize
+	pattern = r'[\s\t.,?!:;\\]+'
+	documents = []
+	stemmer = nltk.stem.PorterStemmer()
+	lemmatizer = nltk.stem.WordNetLemmatizer()
+	
+	for lineNumber in range(0, len(lines), 2):
+		headerWords = re.split(pattern, lines[lineNumber])
+		textWords = re.split(pattern, lines[lineNumber + 1])
+		removeSingleAndEmptyChar(headerWords)
+		removeSingleAndEmptyChar(textWords)
+		document = []
+		document.extend(sanitizeTokens(headerWords, stemmer, lemmatizer))
+		document.extend(sanitizeTokens(textWords, stemmer, lemmatizer))
+		documents.append({word: True for word in document})
+	
+	return list(zip(documents, [assignedClass for document in documents]))
 
-	processed = []		# fill this list with the preprocessed form of the text in the file. you may change to another data structure, if you need. 
-						# do not forget to label your documents. 
-						# if a document from Class1 becomes "world case speak silent" after basic text processing steps; after labeling, it will look like ("world case speak silent", Class1)
-	#...
-	#...
-	#...
-	#...
-	#...
-	#...
-	#...
-	#...
+def checkIfFileExists(filename):
+	return os.path.isfile(filename)
 
-	# At this point, *processed* should look like this: [(("world case speak silent", Class1), ("second docu come here", ClassN), ....., ("another docu continu finish", ClassX), ....]
-	return processed 	# you may change the return value if you need.
-	
-	
-	
-	
-	
-	
-	
-######################################################################################################################################################
-# This part is not compulsory. However, merging preprocessed forms of your files into mega documents may be pretty helpful.
-# You may also want to permanently store (i.e. write to a file) those mega documents so as not to preprocess your file again and again at each trial.
-def create_training_megadoc():
-	training_documents = ["philosophy_train.txt","sports_train.txt","mystery_train.txt","religion_train.txt","science_train.txt","romance_train.txt","horror_train.txt","science-fiction_train.txt"]
+def create_megadoc(env):
+	# Read and saves megadoc
+	megadocFilename = "megadoc_" + env + ".txt"
 	training_megadoc = []
+	if not checkIfFileExists(megadocFilename):
+		genres = ["philosophy","sports","mystery","religion","science","romance","horror","science-fiction"]
+		training_documents = [genre + "_" + env + ".txt" for genre in genres]
+		dirName = "task1/" + env + "/"
 	
-	for filename in training_documents:
-		training_megadoc += preprocess(filename)
-	#####	
-	#...
-	# Here, you may write the training_megadoc to a file. (You may also do it elsewhere or nowhere.)
-	#...
-	#####
+		for index, filename in enumerate(training_documents):
+			training_megadoc.extend(preprocess(dirName + filename, genres[index]))
+
+		with open(megadocFilename , 'w') as fout:
+			json.dump(training_megadoc, fout)
+	else:
+		file = open(megadocFilename)
+		training_megadoc = json.load(file)
+		file.close()
+	
 	return training_megadoc
-	
-	
-def create_test_megadoc():
-	training_documents = ["philosophy_test.txt","sports_test.txt","mystery_test.txt","religion_test.txt","science_test.txt","romance_test.txt","horror_test.txt","science-fiction_test.txt"]	
-	test_megadoc = []
-	#...
-	#...
-	#... *** TO DO ***
-	#...
-	#...
-	return test_megadoc
-		
-####################################################################################################################################################
-
-
-
-
-
 
 def extract_features(megadoc):		# megadoc can be either training_megadoc for training phase or test_megadoc for testing phase.
+	print("extractFeatures")
+	return megadoc
 	####################################################################################################################
 	#																												   #	
 	#		TO DO: Select features and create feature-based representations of labeled documents.                      #
@@ -95,33 +85,28 @@ def extract_features(megadoc):		# megadoc can be either training_megadoc for tra
 	####################################################################################################################
 
 
-
-
-
-
-def train(classifier, training_set):	# classifier is either nltk.NaiveBayesClassifier or SklearnClassifier(SVC()). Example call: train(SklearnClassifier(SVC()), training_set)
-	####################################################################################################################
-	#																												   #	
-	#		TO DO: Use feature-based representations of your labeled documents to train your classifier.			   # 
-	#																												   #
-	####################################################################################################################
-
-	
+def train(classifier, trainingSet):
+	return classifier.train(trainingSet)
 	
 
-	
+def test(classifier, testSet):
+	yTrue = [label for (_, label) in testSet]
+	yPred = [classifier.classify(features) for (features, _) in testSet]
 
-def test(classifier, test_set):
-	####################################################################################################################
-	#																												   #	
-	#		TO DO: Use feature-based representations of your labeled documents to test your trained classifier.		   #
-	#	 Compute accuracy, recall, precision values and confusion matrices. Present and discuss them at your report.   # 
-	#																												   #
-	####################################################################################################################
+	accuracy = accuracy_score(yTrue, yPred)
+	print("Accuracy:", accuracy)	
 
+	recall = recall_score(yTrue, yPred, average="micro")
+	print("Recall:", recall)
 
+	precision = precision_score(yTrue, yPred, average="micro")
+	print("Precision:", precision)
 
+	f1Score = f1_score(yTrue, yPred, average="micro")
+	print("F1-Score:", f1Score)
 
+	print("\Confusion Matrix:")
+	print(confusion_matrix(yTrue, yPred))
 
 
 def save_classifier(classifier, filename):	#filename should end with .pickle and type(filename)=string
@@ -131,24 +116,54 @@ def save_classifier(classifier, filename):	#filename should end with .pickle and
 	
 	
 def load_classifier(filename):	#filename should end with .pickle and type(filename)=string
-	classifier_file = open(filename, "rb")
-	classifier = pickle.load(classifier_file)
-	classifier_file.close()
-	return classifier
+	if checkIfFileExists(filename):
+		classifier_file = open(filename, "rb")
+		classifier = pickle.load(classifier_file)
+		classifier_file.close()
+		return classifier
+	else:
+		return False
+	
+def getClassifier(classifierName, trainingSet, env):
+	filename = classifierName + "_" + env
+	if classifierName == "naive":
+		naiveBayesClassifier = load_classifier(filename)
+		if not naiveBayesClassifier:
+			naiveBayesClassifier = nltk.NaiveBayesClassifier
+			trainedNaiveBayesClassifier = train(naiveBayesClassifier, trainingSet)
+			save_classifier(trainedNaiveBayesClassifier, filename)
+			return trainedNaiveBayesClassifier
+		else:
+			return naiveBayesClassifier
+	elif classifierName == "svc":
+		svcClassifier = load_classifier(filename)
+		if not svcClassifier:
+			svcClassifier = nltk.classify.SklearnClassifier(SVC())
+			train(svcClassifier, trainingSet)
+			save_classifier(svcClassifier, filename)
 
-
+		return svcClassifier	
+	else:
+		raise Exception("Unknown classifier name:" + classifierName)
 
 
 if __name__ == "__main__":
-	# You may add or delete global variables.
-	training_set = []
-	test_set = []
-	
-	##############################
-	#							 #
-	#	 THE STAGE IS YOURS!	 #	
-	#							 #
-	##############################
+	env = "train" # train or dev
+	trainingSet = create_megadoc(env)
+	testSet = create_megadoc("test")
+
+	trainingSet = extract_features(trainingSet)
+	testSet = extract_features(testSet)
+
+	naiveClassifier = getClassifier("naive", trainingSet, env)
+	svcClassifier = getClassifier("svc", trainingSet, env)
+
+	print("Naive Bayes:")
+	test(naiveClassifier, testSet)
+
+	print("Support Vector Classifier:")
+	test(svcClassifier, testSet)
+
 
 
 
