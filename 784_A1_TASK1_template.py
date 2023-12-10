cb7295ad4fa207460084754183603f16a9829d9c
Can Ozsoy <cozsoy@altayer.com>
2023-12-10 11:57:21 +0300

    variable naming changed

--- 784_A1_TASK1_template.py
+++ 784_A1_TASK1_template.py
@@ -1,34 +1,31 @@
 from typing import List
 import nltk
-from sklearn.metrics import accuracy_score, confusion_matrix, recall_score, precision_score, f1_score
 import pickle	# this is for saving and loading your trained classifiers.
 import re
 from nltk.corpus import stopwords
 from sklearn.svm import SVC
 import json
 import os
-
+from sklearn import metrics
 
 nltk.download("wordnet")
 nltk.download('stopwords')
-
-stopWords = set(stopwords.words("english"))
 	
-def removeSingleAndEmptyChar(words: List[str]):
-	singleCharacterPattern = r'^[\w]{1}$'
-	emptyStringPattern = r'^$'
+def remove_single_and_emoty_char(words: List[str]):
+	single_character_pattern = r'^[\w]{1}$'
+	empty_string_pattern = r'^$'
 	for word in words:
-		if re.search(singleCharacterPattern, word) or re.search(emptyStringPattern, word):
+		if re.search(single_character_pattern, word) or re.search(empty_string_pattern, word):
 			words.remove(word)	
 	
-def lowercaseStemAndLemmatize(token, stemmer, lemmatizer):
+def lowercase_stem_and_lemmatize(token: str, stemmer, lemmatizer):
 	return lemmatizer.lemmatize(stemmer.stem(token.lower()))
 
-def sanitizeTokens(tokens: List[str], stemmer, lemmatizer):
-	return [lowercaseStemAndLemmatize(token, stemmer, lemmatizer) for token in tokens if token.isalpha() and token.lower() not in stopWords]
+def sanitize_tokens(tokens: List[str], stemmer, lemmatizer):
+	return [lowercase_stem_and_lemmatize(token, stemmer, lemmatizer) for token in tokens if token.isalpha() and token.lower() not in stop_words]
 	
 
-def preprocess(filename, assignedClass):				
+def pre_process(filename, assigned_class):			
 	file = open(filename, 'r')			
 	lines = file.read().splitlines()
 	file.close()
@@ -39,37 +36,37 @@ def preprocess(filename, assignedClass):
 	stemmer = nltk.stem.PorterStemmer()
 	lemmatizer = nltk.stem.WordNetLemmatizer()
 	
-	for lineNumber in range(0, len(lines), 2):
-		headerWords = re.split(pattern, lines[lineNumber])
-		textWords = re.split(pattern, lines[lineNumber + 1])
-		removeSingleAndEmptyChar(headerWords)
-		removeSingleAndEmptyChar(textWords)
+	for line_number in range(0, len(lines), 2):
+		header_words = re.split(pattern, lines[line_number])
+		text_words = re.split(pattern, lines[line_number + 1])
+		remove_single_and_emoty_char(header_words)
+		remove_single_and_emoty_char(text_words)
 		document = []
-		document.extend(sanitizeTokens(headerWords, stemmer, lemmatizer))
-		document.extend(sanitizeTokens(textWords, stemmer, lemmatizer))
+		document.extend(sanitize_tokens(header_words, stemmer, lemmatizer))
+		document.extend(sanitize_tokens(text_words, stemmer, lemmatizer))
 		documents.append({word: True for word in document})
 	
-	return list(zip(documents, [assignedClass for document in documents]))
+	return list(zip(documents, [assigned_class for document in documents]))
 
-def checkIfFileExists(filename):
+def check_if_file_exists(filename):
 	return os.path.isfile(filename)
 
 def create_megadoc(env):
 	# Read and saves megadoc
-	megadocFilename = "megadoc_" + env + ".txt"
+	megadoc_filename = "megadoc_" + env + ".txt"
 	training_megadoc = []
-	if not checkIfFileExists(megadocFilename):
+	if not check_if_file_exists(megadoc_filename):
 		genres = ["philosophy","sports","mystery","religion","science","romance","horror","science-fiction"]
 		training_documents = [genre + "_" + env + ".txt" for genre in genres]
-		dirName = "task1/" + env + "/"
+		dir_name = "task1/" + env + "/"
 	
 		for index, filename in enumerate(training_documents):
-			training_megadoc.extend(preprocess(dirName + filename, genres[index]))
+			training_megadoc.extend(pre_process(dir_name + filename, genres[index]))
 
-		with open(megadocFilename , 'w') as fout:
+		with open(megadoc_filename , 'w') as fout:
 			json.dump(training_megadoc, fout)
 	else:
-		file = open(megadocFilename)
+		file = open(megadoc_filename)
 		training_megadoc = json.load(file)
 		file.close()
 	
@@ -85,38 +82,38 @@ def extract_features(megadoc):		# megadoc can be either training_megadoc for tra
 	####################################################################################################################
 
 
-def train(classifier, trainingSet):
-	return classifier.train(trainingSet)
+def train(classifier, training_set):
+	return classifier.train(training_set)
 	
 
-def test(classifier, testSet):
-	yTrue = [label for (_, label) in testSet]
-	yPred = [classifier.classify(features) for (features, _) in testSet]
+def test(classifier, test_set):
+	y_true = [label for (_, label) in test_set]
+	y_pred = [classifier.classify(features) for (features, _) in test_set]
 
-	accuracy = accuracy_score(yTrue, yPred)
+	accuracy = metrics.accuracy_score(y_true, y_pred)
 	print("Accuracy:", accuracy)	
 
-	recall = recall_score(yTrue, yPred, average="micro")
+	recall = metrics.recall_score(y_true, y_pred, average="micro")
 	print("Recall:", recall)
 
-	precision = precision_score(yTrue, yPred, average="micro")
+	precision = metrics.precision_score(y_true, y_pred, average="micro")
 	print("Precision:", precision)
 
-	f1Score = f1_score(yTrue, yPred, average="micro")
+	f1Score = metrics.f1_score(y_true, y_pred, average="micro")
 	print("F1-Score:", f1Score)
 
 	print("\Confusion Matrix:")
-	print(confusion_matrix(yTrue, yPred))
+	print(metrics.confusion_matrix(y_true, y_pred))
 
 
-def save_classifier(classifier, filename):	#filename should end with .pickle and type(filename)=string
+def save_classifier(classifier, filename: str):
 	with open(filename, "wb") as f:
 		pickle.dump(classifier, f)
 	return
 	
 	
-def load_classifier(filename):	#filename should end with .pickle and type(filename)=string
-	if checkIfFileExists(filename):
+def load_classifier(filename: str):
+	if check_if_file_exists(filename):
 		classifier_file = open(filename, "rb")
 		classifier = pickle.load(classifier_file)
 		classifier_file.close()
@@ -124,45 +121,46 @@ def load_classifier(filename):	#filename should end with .pickle and type(filena
 	else:
 		return False
 	
-def getClassifier(classifierName, trainingSet, env):
-	filename = classifierName + "_" + env
-	if classifierName == "naive":
-		naiveBayesClassifier = load_classifier(filename)
-		if not naiveBayesClassifier:
-			naiveBayesClassifier = nltk.NaiveBayesClassifier
-			trainedNaiveBayesClassifier = train(naiveBayesClassifier, trainingSet)
-			save_classifier(trainedNaiveBayesClassifier, filename)
-			return trainedNaiveBayesClassifier
+def getClassifier(classifier_name: str, training_set, env: str):
+	filename = classifier_name + "_" + env
+	if classifier_name == "naive":
+		naive_bayes_classifier = load_classifier(filename)
+		if not naive_bayes_classifier:
+			naive_bayes_classifier = nltk.NaiveBayesClassifier
+			trained_naive_bayes_classifier = train(naive_bayes_classifier, training_set)
+			save_classifier(trained_naive_bayes_classifier, filename)
+			return trained_naive_bayes_classifier
 		else:
-			return naiveBayesClassifier
-	elif classifierName == "svc":
-		svcClassifier = load_classifier(filename)
-		if not svcClassifier:
-			svcClassifier = nltk.classify.SklearnClassifier(SVC())
-			train(svcClassifier, trainingSet)
-			save_classifier(svcClassifier, filename)
+			return trained_naive_bayes_classifier
+	elif classifier_name == "svc":
+		svc_classifier = load_classifier(filename)
+		if not svc_classifier:
+			svc_classifier = nltk.classify.SklearnClassifier(SVC())
+			train(svc_classifier, training_set)
+			save_classifier(svc_classifier, filename)
 
-		return svcClassifier	
+		return svc_classifier	
 	else:
-		raise Exception("Unknown classifier name:" + classifierName)
+		raise Exception("Unknown classifier name:" + classifier_name)
 
 
 if __name__ == "__main__":
+	stop_words = set(stopwords.words("english"))
 	env = "train" # train or dev
-	trainingSet = create_megadoc(env)
-	testSet = create_megadoc("test")
+	training_set = create_megadoc(env)
+	test_set = create_megadoc("test")
 
-	trainingSet = extract_features(trainingSet)
-	testSet = extract_features(testSet)
+	training_set = extract_features(training_set)
+	test_set = extract_features(test_set)
 
-	naiveClassifier = getClassifier("naive", trainingSet, env)
-	svcClassifier = getClassifier("svc", trainingSet, env)
+	naive_classifier = getClassifier("naive", training_set, env)
+	svc_classifier = getClassifier("svc", training_set, env)
 
 	print("Naive Bayes:")
-	test(naiveClassifier, testSet)
+	test(naive_classifier, test_set)
 
 	print("Support Vector Classifier:")
-	test(svcClassifier, testSet)
+	test(svc_classifier, test_set)
 
 
 
